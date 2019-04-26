b3dc3b99489ff09959e4af35fc51dc7a9e5cbef9
silvicardo <silvicardo@gmail.com>
2019-04-26 11:41:58 +0200

    passport

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Auth::routes();
 Route::get('/', 'ChatsController@index');
 Route::get('messages', 'ChatsController@fetchMessages');
 Route::post('messages', 'ChatsController@sendMessage');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
